82bb19a85e0a76d687ec6638e8bed3791d80ebf9
Eric Loots <eric.loots@gmail.com>
2016-11-08 12:25:21 +0100

    Remove trailing code

--- common/src/main/scala/com/lightbend/demo/akkastreams/package.scala
+++ common/src/main/scala/com/lightbend/demo/akkastreams/package.scala
@@ -16,9 +16,6 @@ package object akkastreams {
   type Iterable[+A] = scala.collection.immutable.Iterable[A]
   val Iterable = scala.collection.immutable.Iterable
 
-//  type Iterator[+A] = scala.collection.immutable.Iterator[A]
-//  val Iterator = scala.collection.immutable.Iterator
-
   case class FilterStage(delay: Int, coefficient: Double)
 
   implicit class FilterStageOps(val s: (Int, Double)) extends AnyVal {
