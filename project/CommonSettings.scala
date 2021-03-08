3d04b068eacb72bdde02f3016e48dbedf431f17b
eloots <eric.loots@gmail.com>
2021-03-08 18:06:17 +0100

    Remove Eclipse plugin from build definition

--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -1,4 +1,3 @@
-import com.typesafe.sbteclipse.core.EclipsePlugin.{EclipseCreateSrc, EclipseKeys}
 import sbt.Keys._
 import sbt._
 import sbtstudent.AdditionalSettings
@@ -11,11 +10,6 @@ object CommonSettings {
     scalacOptions ++= CompileOptions.compileOptions,
     unmanagedSourceDirectories in Compile := List((scalaSource in Compile).value, (javaSource in Compile).value),
     unmanagedSourceDirectories in Test := List((scalaSource in Test).value, (javaSource in Test).value),
-    EclipseKeys.createSrc := EclipseCreateSrc.Default + EclipseCreateSrc.ManagedClasses,
-    EclipseKeys.eclipseOutput := Some(".target"),
-    EclipseKeys.withSource := true,
-    EclipseKeys.skipParents in ThisBuild := true,
-    EclipseKeys.skipProject := true,
     fork in Test := true,
     parallelExecution in Test := false,
     logBuffered in Test := false,
